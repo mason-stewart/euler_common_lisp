e9ea3170126cd9a2be49caf0a39f33fd091f0b67
Mason Stewart <masondesu@gmail.com>
2013-02-17 22:51:20 -0500

    Renamed 002\'s main function for clarity

--- 002.lisp
+++ 002.lisp
@@ -2,12 +2,12 @@
 ; By considering the terms in the Fibonacci sequence whose values
 ; do not exceed four million, find the sum of the even-valued terms.
 
-; usage: (SUM-FIBONACCI 4000000)
-(defun sum-fibonacci (limit &optional (numbers '(1 2)))
+; usage: (SUM-EVEN-FIBONACCI 4000000)
+(defun sum-even-fibonacci (limit &optional (numbers '(1 2)))
 
   (cond ((< (car (last numbers)) limit)
-          (sum-fibonacci limit (append numbers
-                                       (list (apply '+ (last numbers 2))))))
+          (sum-even-fibonacci limit (append numbers
+                                            (list (apply '+ (last numbers 2))))))
         (t (print `(oh fun! the total was
               ,(apply '+ (mapcan (lambda (number)
                                    (if (eq (mod number 2) 0)
